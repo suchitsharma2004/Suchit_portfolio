095244885ebb18711feeef843c788da4e4dec2ef
Suchit Sharma <suchitsharma@Suchits-MacBook-Air.local>
2024-01-29 20:26:56 +0530

    completed

--- src/main.js
+++ src/main.js
@@ -19,3 +19,60 @@ tabs.forEach((tab)=>{
 
     });
 });
+newtab=document.getElementsByClassName("tab_btn");
+console.log(newtab);
+
+
+///////////////////////////contact form///////////////////////////////
+
+const contactform=document.getElementById('contact-form'),
+contactName=document.getElementById('contact-name'),
+contactEmail=document.getElementById('contact-email'),
+contactSubject=document.getElementById('contact-subject'),
+contactMessage=document.getElementById('contact-message'),
+errorMessage=document.getElementById('error-message')
+
+const sendEmail= (e)=>{
+    e.preventDefault();
+
+    if(
+        contactName.value ==='' ||
+        contactEmail.value==='' ||
+        contactMessage.value===''||
+        contactSubject.value===''
+    ){
+        errorMessage.textContent='Fill all element fields';
+    }
+    else{
+         emailjs.sendForm('service_iteofze',
+         'template_vhxpxjw',
+         '#contact-form',
+         'nIUvqjIIHGnpVKe4m').then(()=>{
+         errorMessage.classList.add('color-first');
+         errorMessage.textContent='Message sent ✅';
+
+         setTimeout(() => {
+            errorMessage.textContent='';
+         }, 5000);
+         }, (error)=>{
+            alert('OOPS! Something went wrong....', error);
+         })
+
+         contactName.value ='';
+         contactEmail.value ='';
+         contactSubject.value ='';
+         contactMessage.value ='';
+
+    }
+};
+contactform.addEventListener('submit',sendEmail);
+
+
+//////////////////////////////////////////////////////////////
+
+function scrollHeader() {
+    const header = document.getElementById('header');
+    if (this.scrollY >= 80) header.classList.add('scroll-header');
+    else header.classList.remove( 'scroll-header');
+}
+    window. addEventListener('scroll', scrollHeader);
